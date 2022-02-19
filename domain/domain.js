ed30f302bedca4fd6f974e6a0f7bd4051985a463
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2022-02-19 12:21:48 +0800

    redep

--- domain/domain.js
+++ domain/domain.js
@@ -1,7 +1,7 @@
 const domains = {
    localhost: "http://localhost:3500",
    network: "https://192.168.254.179:3500",
-   productions: "https://api-searchnstay.herokuapp.com",
+   production: "https://api-searchnstay.herokuapp.com",
 };
 
-module.exports = domains.productions;
+module.exports = domains.production;
