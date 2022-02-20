1958bee4b95e441cf1f01daab51c49d977761d98
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2022-02-20 19:05:03 +0800

    setup local

--- domain/domain.js
+++ domain/domain.js
@@ -1,6 +1,6 @@
 const domains = {
    localhost: "http://localhost:3500",
-   network: "https://192.168.254.179:3500",
+   network: "http://192.168.254.179:3500",
    production: "https://api-searchnstay.herokuapp.com",
 };
 
